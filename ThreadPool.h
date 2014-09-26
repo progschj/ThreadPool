9a42ec1329f259a5f4881a291db1dcb8f2ad9040
Jakob Progsch <jakob@progsch.net>
2014-09-26 12:32:41 +0200

    changed typedef to using

--- ThreadPool.h
+++ ThreadPool.h
@@ -63,7 +63,7 @@ template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
-    typedef typename std::result_of<F(Args...)>::type return_type;
+    using return_type = typename std::result_of<F(Args...)>::type;
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
@@ -91,7 +91,7 @@ inline ThreadPool::~ThreadPool()
         stop = true;
     }
     condition.notify_all();
-    for(std::thread & worker: workers)
+    for(std::thread &worker: workers)
         worker.join();
 }
 
