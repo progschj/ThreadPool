a7d611f2c08383e20008f564d3085b279cadba9d
Youka <spanknebel.borken@t-online.de>
2015-07-10 17:26:40 +0200

    changed stop flag to atomic type
    Atomics are cheaper for thread safety

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -7,6 +7,7 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <atomic>
 #include <future>
 #include <functional>
 #include <stdexcept>
@@ -43,6 +44,10 @@ public:
     template<class F, class... Args>
     std::future<typename std::result_of<F(Args...)>::type> enqueue(F&& f, Args&&... args)
     {
+        // don't allow enqueueing after stopping the pool
+        if(stop)
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+
         using return_type = typename std::result_of<F(Args...)>::type;
 
         auto task = std::make_shared< std::packaged_task<return_type()> >(
@@ -52,11 +57,6 @@ public:
         std::future<return_type> res = task->get_future();
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-
-            // don't allow enqueueing after stopping the pool
-            if(stop)
-                throw std::runtime_error("enqueue on stopped ThreadPool");
-
             tasks.emplace([task](){ (*task)(); });
         }
         condition.notify_one();
@@ -65,10 +65,7 @@ public:
     // the destructor joins all threads
     ~ThreadPool()
     {
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            stop = true;
-        }
+        stop = true;
         condition.notify_all();
         for(std::thread &worker: workers)
             worker.join();
@@ -82,7 +79,7 @@ private:
     // synchronization
     std::mutex queue_mutex;
     std::condition_variable condition;
-    bool stop;
+    std::atomic_bool stop;
 };
 
 #endif
