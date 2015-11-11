e463df9f059f151291235b0f2d77f635be2bb714
Steven LeMay <steven.lemay@sbcglobal.net>
2015-11-11 08:52:55 -0800

    Break into header and implementation
    Break the source into header and implementation, make the template
    definition more readable by changing F to Callable

---
+++ ThreadPool.cpp
@@ -0,0 +1,61 @@
+// Purpose: Thread pool
+
+// Based on https://github.com/progschj/ThreadPool
+
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool (size_t threads)
+{
+   workers.reserve (threads);
+
+   for (size_t count = 0; count < threads; ++count)
+   {
+      // Worker execution loop
+      workers.emplace_back ([this]()
+      {
+         for (;;)
+         {
+            // Task to execute
+            std::function<void ()> task;
+
+            // Wait for additional work signal
+            { // Critical section
+               // Wait to be notified of work
+               Lock lock (this->queue_mutex);
+               this->condition.wait (lock, [this]()
+               { 
+                  return this->stop || !this->tasks.empty (); 
+               });
+
+               // Exit the thread if stopping and no work remains
+               if (this->stop && this->tasks.empty ())
+                  return;
+
+               // Dequeue the next task
+               task = std::move (this->tasks.front ());
+               this->tasks.pop ();
+            } // End critical section
+
+            // Execute
+            task ();
+         }
+      });
+   }
+}
+
+// Destructor joins all worker threads
+ThreadPool::~ThreadPool ()
+{
+   { // Critical section
+      Lock lock (queue_mutex);
+      stop = true;
+   } // End critical section
+
+   condition.notify_all ();
+
+   // Wait for threads to complete work
+   for (std::thread &worker : workers)
+   {
+      if (worker.joinable ()) { worker.join(); }
+   }
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -16,138 +16,86 @@
 
 class ThreadPool final
 {
-  public:
-	 ThreadPool (size_t threads);
-	 ~ThreadPool ();
+   public:
+      // Launches specified number of worker threads
+      ThreadPool (size_t threads);
+      ~ThreadPool ();
 
-	// Enqueue task and return std::future<>
-	 template<class F, class... Args>
-	 auto enqueue (F&& f, Args&&... args) 
-		-> std::future<typename std::result_of<F (Args...)>::type>;
+      // Not copyable
+      ThreadPool (const ThreadPool &) = delete;
+      ThreadPool& operator= (const ThreadPool &) = delete;
 
-	// Enqueue task without requiring a return value
-	 template<class F, class... Args>
-	 auto enqueueAndDetach (F&& f, Args&&... args) 
-		-> void;
+      // Not moveable
+      ThreadPool (ThreadPool &&) = delete;
+      ThreadPool& operator= (const ThreadPool &&) = delete;
 
-  private:
-	 // Keep track of threads, so they can be joined
-	 std::vector<std::thread> workers;
-	 // Task queue
-	 std::queue<std::function<void ()>> tasks;
+      // Enqueue task and return std::future<>
+      template<class Callable, class... Args>
+      auto enqueue (Callable&& callable, Args&&... args) 
+         -> std::future<typename std::result_of<Callable (Args...)>::type>;
 
-	 // Synchronization
-	 using Lock = std::unique_lock<std::mutex>;
-	 std::mutex queue_mutex;
-	 std::condition_variable condition;
-	 bool stop = false;
+      // Enqueue task without requiring an std::future<>
+      template<class Callable, class... Args>
+      auto enqueueAndDetach (Callable&& callable, Args&&... args) 
+         -> void;
+
+   private:
+      // Keep track of threads, so they can be joined
+      std::vector<std::thread> workers;
+      // Task queue
+      std::queue<std::function<void ()>> tasks;
+ 
+      // Synchronization
+      using Lock = std::unique_lock<std::mutex>;
+      std::mutex queue_mutex;
+      std::condition_variable condition;
+      bool stop = false;
 };
 
-// the constructor just launches some amount of workers
-ThreadPool::ThreadPool (size_t threads)
+// Add a new work item to the pool
+template<class Callable, class... Args>
+auto ThreadPool::enqueue (Callable&& callable, Args&&... args) 
+   -> std::future<typename std::result_of<Callable (Args...)>::type>
 {
-  for (size_t i = 0; i < threads; ++i)
-  {
-	 // Worker execution loop
-	 workers.emplace_back
-	 (
-		[this]
-		{
-		   for(;;)
-		   {
-			  // Task to execute
-			  std::function<void ()> task;
+   using return_t = typename std::result_of<Callable (Args...)>::type;
+   using task_t = std::packaged_task<return_t ()>;
 
-			  // Wait for additional work signal
-			  { // Critical section
-				 // Wait to be notified of work
-				 Lock lock (this->queue_mutex);
-				 this->condition.wait
-				 (
-					lock,
-					[this](){ return this->stop || !this->tasks.empty (); }
-				 );
+   auto task = std::make_shared<task_t> (std::bind (std::forward<Callable> (callable), std::forward<Args> (args)...));
+   std::future<return_t> result = task->get_future();
 
-				 // Exit the thread if stopping and no work remains
-				 if(this->stop && this->tasks.empty ())
-					return;
+   { // Critical section
+      Lock lock (queue_mutex);
 
-				 // Dequeue the next task
-				 task = std::move (this->tasks.front ());
-				 this->tasks.pop ();
-			  } // End critical section
+      // Don't allow an enqueue after stopping
+      if (stop)
+         throw std::runtime_error ("enqueue on stopped ThreadPool");
 
-			  // Execute
-			  task ();
-		   }
-		}
-	 );
-  }
+      // Push work back on the queue
+      tasks.emplace ([task](){ (*task)(); });
+   } // End critical section
+
+   // Notify a thread that there is new work to perform
+   condition.notify_one ();
+   return result;
 }
 
 // Add a new work item to the pool
-template<class F, class... Args>
-auto ThreadPool::enqueue (F&& f, Args&&... args) 
-  -> std::future<typename std::result_of<F (Args...)>::type>
+template<class Callable, class... Args>
+auto ThreadPool::enqueueAndDetach (Callable&& callable, Args&&... args) 
+   -> void
 {
-  using return_type = typename std::result_of<F(Args...)>::type;
+   { // Critical section
+      Lock lock (queue_mutex);
 
-  auto task = std::make_shared<std::packaged_task<return_type ()>>
-  (
-	 std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-  );
-	
-  std::future<return_type> result = task->get_future();
+      // Don't allow an enqueue after stopping
+      if (stop)
+         throw std::runtime_error ("enqueue on stopped ThreadPool");
 
-  { // Critical section
-	 Lock lock (queue_mutex);
+      // Push work back on the queue
+      tasks.emplace (std::bind (std::forward<Callable> (callable), std::forward<Args> (args)...));
+   } // End critical section
 
-	 // Don't allow an enqueue after stopping
-	 if (stop)
-		throw std::runtime_error ("enqueue on stopped ThreadPool");
-
-	 // Push work back on the queue
-	 tasks.emplace ([task](){ (*task)(); });
-  } // End critical section
-
-  // Notify a thread that there is new work to perform
-  condition.notify_one ();
-  return result;
+   // Notify a thread that there is new work to perform
+   condition.notify_one ();
 }
 
-// Add a new work item to the pool
-template<class F, class... Args>
-auto ThreadPool::enqueueAndDetach (F&& f, Args&&... args) 
-  -> void
-{
-  { // Critical section
-	 Lock lock (queue_mutex);
-
-	 // Don't allow an enqueue after stopping
-	 if (stop)
-		throw std::runtime_error ("enqueue on stopped ThreadPool");
-
-	 // Push work back on the queue
-	 tasks.emplace (std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-  } // End critical section
-
-  // Notify a thread that there is new work to perform
-  condition.notify_one ();
-}
-
-// Destructor joins all worker threads
-ThreadPool::~ThreadPool ()
-{
-  { // Critical section
-	 Lock lock (queue_mutex);
-	 stop = true;
-  } // End critical section
-
-  condition.notify_all ();
-
-  // Wait for threads to complete work
-  for (std::thread &worker : workers)
-  {
-	 if (worker.joinable ()) { worker.join(); }
-  }
-}
