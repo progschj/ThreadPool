36cb156bac96e57058066be875b381fba4aabf00
WillBrennan <WillBrennan@users.noreply.github.com>
2017-01-17 22:55:26 +0000

    added travis and moved around

--- examples/example.cpp
+++ examples/example.cpp
@@ -2,28 +2,24 @@
 #include <vector>
 #include <chrono>
 
-#include "ThreadPool.h"
+#include "thread_pool.h"
 
-int main()
-{
-    
+int main(int argc, char** argv) {
     ThreadPool pool(4);
     std::vector< std::future<int> > results;
 
     for(int i = 0; i < 8; ++i) {
         results.emplace_back(
             pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
                 return i*i;
             })
         );
     }
 
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
+    for(auto && result: results) {
+        std::cout << result.get() << std::endl;
+    }
     
     return 0;
 }
---
+++ tests/test_thread_pool.cc
@@ -0,0 +1,55 @@
+#include <array>
+#include <future>
+#include <string>
+#include <vector>
+
+#include "thread_pool.h"
+
+#include "gtest/gtest.h"
+
+TEST(ThreadPool, Constructors) {
+    EXPECT_NO_THROW(ThreadPool());
+    EXPECT_NO_THROW(ThreadPool(1));
+    EXPECT_THROW(ThreadPool(0), std::runtime_error);
+}
+
+TEST(ThreadPool, Enqueue) {
+    ThreadPool pool;
+
+    ASSERT_NO_THROW(pool.enqueue([](const std::string& i) { return i + " " + i; }, "kitten"));
+
+    ASSERT_NO_THROW(pool.enqueue([](const int& i) { return 12 * i; }, 3));
+    ASSERT_NO_THROW(pool.enqueue([](const float& i) { return 0.1f * i; }, 1.38f));
+    ASSERT_NO_THROW(pool.enqueue([](const double& i) { return 0.1 * i; }, 1.38));
+
+    auto result_a = pool.enqueue([](const std::string& i) { return i + " " + i; }, "kitten").get();
+    EXPECT_EQ(result_a, "kitten kitten");
+
+    auto result_b = pool.enqueue([](const int& i) { return 12 * i; }, 3).get();
+    EXPECT_EQ(result_b, 36);
+
+    auto result_c = pool.enqueue([](const float& i) { return 0.1f * i; }, 1.38f).get();
+    EXPECT_NEAR(result_c, 0.138f, 1E-4);
+
+    auto result_d = pool.enqueue([](const double& i) { return 0.1 * i; }, 1.38).get();
+    EXPECT_NEAR(result_d, 0.138, 1E-4);
+}
+
+TEST(ThreadPool, EnqueueExample) {
+    ThreadPool pool;
+
+    std::vector<std::future<int>> results_int;
+    results_int.reserve(12);
+    for (auto i = 0; i < 12; ++i) {
+        results_int.emplace_back(pool.enqueue([](auto i) { return i * i; }, i));
+    }
+
+    auto sum = 0;
+    for (auto&& result : results_int) {
+        sum += result.get();
+    }
+
+    EXPECT_EQ(sum, 506);
+
+    EXPECT_NO_THROW(pool.enqueue([](const std::string& i) { return "the " + i + " says hello"; }, "cat"));
+}
