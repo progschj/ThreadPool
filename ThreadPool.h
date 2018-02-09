dde78d4908999d6163f0afaf3375b923326d33f7
danilcha <danilcha@gmail.com>
2018-02-09 20:26:13 +0100

    Remove 'inline' from the destructor

--- ThreadPool.h
+++ ThreadPool.h
@@ -84,7 +84,7 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 }
 
 // the destructor joins all threads
-inline ThreadPool::~ThreadPool()
+ThreadPool::~ThreadPool()
 {
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
